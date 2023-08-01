193201d5d57635bdf503e2d1eae7dd693f8904b5
marimeireles <marianameireles@protonmail.com>
2023-08-01 16:56:37 -0400

    Add the ability of showing roman chars

--- src/recurse_browser/recurse_window.py
+++ src/recurse_browser/recurse_window.py
@@ -1,13 +1,8 @@
 import tkinter
 
+from recurse_browser.browser_config import WIDTH, HEIGHT, HSTEP, VSTEP, SCROLL_STEP
 from recurse_browser.recurse_requests import request
-
-# from recurse_requests import request
-
-WIDTH, HEIGHT = 800, 600
-HSTEP, VSTEP = 13, 18  # Horizontal and vertical pixel sizes
-SCROLL_STEP = 100
-
+from recurse_browser.text_formatting import lex, Layout
 
 class Browser:
     def __init__(self):
@@ -23,22 +18,24 @@ class Browser:
         Loops through each tuple in display_list and draws
         character at its x,y coordinates
         """
+        # erases canvas before text is redrawn during scrolling
         self.canvas.delete(
             "all"
-        )  # erases canvas before text is redrawn during scrolling
-        for x, y, c in self.display_list:
+        )
+
+        for x, y, word, font in self.display_list:
             # performance enhancement, prevents redrawing pixels that are off screen
             if y > self.scroll + HEIGHT:
                 continue  # skips character below viewing window
             if y + VSTEP < self.scroll:
                 continue  # skips character above viewing window
 
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            self.canvas.create_text(x, y - self.scroll, text=word, font=font, anchor="nw")
 
     def load(self, url):
         headers, body = request(url)
-        text = lex(body)
-        self.display_list = layout(text)
+        tokens = lex(body)
+        self.display_list = Layout(tokens).display_list
         self.draw()
 
     def scrolldown(
@@ -46,36 +43,3 @@ class Browser:
     ):  # e is an event object that's passed to Tk, but isn't used here
         self.scroll += SCROLL_STEP
         self.draw()
-
-
-def layout(text):
-    """
-    Computes and stores the position of each character in text
-    as a tuple in display_list
-    """
-    display_list = []
-    cursor_x, cursor_y = HSTEP, VSTEP
-    for c in text:
-        display_list.append((cursor_x, cursor_y, c))
-        cursor_x += HSTEP
-        if cursor_x >= WIDTH - HSTEP:
-            cursor_y += VSTEP  # Breaks onto next line
-            cursor_x = HSTEP  # Resets horizontal cursor position
-    return display_list
-
-
-def lex(body):
-    """
-    Strips tags (anything between angle brackets) from body,
-    accumulates and and returns the remaining text.
-    """
-    text = ""
-    in_angle = False
-    for c in body:
-        if c == "<":
-            in_angle = True
-        elif c == ">":
-            in_angle = False
-        elif not in_angle:
-            text += c
-    return text
---
+++ src/recurse_browser/text_formatting.py
@@ -0,0 +1,117 @@
+import tkinter.font
+
+from recurse_browser.browser_config import WIDTH, HEIGHT, HSTEP, VSTEP, SCROLL_STEP
+from recurse_browser.recurse_requests import request
+
+class Text:
+    def __init__(self, text):
+        self.text = text
+
+    def __repr__(self):
+        return "Text('{}')".format(self.text)
+
+class Tag:
+    def __init__(self, tag):
+        self.tag = tag
+
+    def __repr__(self):
+        return "Tag('{}')".format(self.tag)
+
+def lex(body):
+    """
+    Strips tags (anything between angle brackets) from body,
+    accumulates and and returns the remaining text.
+    """
+    out = []
+    text = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+            if text: out.append(Text(text))
+            text = ""
+        elif c == ">":
+            in_tag = False
+            out.append(Tag(text))
+            text = ""
+        else:
+            text += c
+    if not in_tag and text:
+        out.append(Text(text))
+    return out
+
+FONTS = {}
+
+def get_font(size, weight, slant):
+    key = (size, weight, slant)
+    if key not in FONTS:
+        font = tkinter.font.Font(size=size, weight=weight, slant=slant)
+        FONTS[key] = font
+    return FONTS[key]
+
+class Layout:
+    """
+    Computes and stores the position of each character in text
+    as a tuple in display_list
+    """
+    def __init__(self, tokens):
+        self.tokens = tokens
+        self.display_list = []
+
+        self.cursor_x = HSTEP  # Resets horizontal cursor position
+        self.cursor_y = VSTEP  # Breaks onto next line
+        self.weight = "normal"
+        self.style = "roman"
+        self.size = 16
+
+        self.line = []
+        for tok in tokens:
+            self.token(tok)
+        self.flush()
+
+    def token(self, tok):
+        if isinstance(tok, Text):
+            self.text(tok)
+        elif tok.tag == "i":
+            self.style = "italic"
+        elif tok.tag == "/i":
+            self.style = "roman"
+        elif tok.tag == "b":
+            self.weight = "bold"
+        elif tok.tag == "/b":
+            self.weight = "normal"
+        elif tok.tag == "small":
+            self.size -= 2
+        elif tok.tag == "/small":
+            self.size += 2
+        elif tok.tag == "big":
+            self.size += 4
+        elif tok.tag == "/big":
+            self.size -= 4
+        elif tok.tag == "br":
+            self.flush()
+        elif tok.tag == "/p":
+            self.flush()
+            self.cursor_y += VSTEP
+        
+    def text(self, tok):
+        font = get_font(self.size, self.weight, self.style)
+        for word in tok.text.split():
+            w = font.measure(word)
+            if self.cursor_x + w > WIDTH - HSTEP:
+                self.flush()
+            self.line.append((self.cursor_x, word, font))
+            self.cursor_x += w + font.measure(" ")
+
+    def flush(self):
+        if not self.line: return
+        metrics = [font.metrics() for x, word, font in self.line]
+        max_ascent = max([metric["ascent"] for metric in metrics])
+        baseline = self.cursor_y + 1.25 * max_ascent
+        for x, word, font in self.line:
+            y = baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
+        self.cursor_x = HSTEP
+        self.line = []
+        max_descent = max([metric["descent"] for metric in metrics])
+        self.cursor_y = baseline + 1.25 * max_descent
