bc2b6278dac917ac9125d61717b691762afb8c21
Thelma Boamah <thelma.a.boamah@gmail.com>
2023-07-31 23:32:14 -0400

    start browser in main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from recurse_browser import recurse_requests
+import tkinter
+from recurse_browser import recurse_window
+
 
 if __name__ == "__main__":
     import sys
-    recurse_requests.load(sys.argv[1])
+
+    recurse_window.Browser().load(sys.argv[1])
+    tkinter.mainloop()
