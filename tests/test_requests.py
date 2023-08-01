17a826c9497adca98ad45a39c15b8921497ccfd8
marimeireles <marianameireles@protonmail.com>
2023-08-01 12:41:47 -0400

    Linting

--- tests/test_requests.py
+++ tests/test_requests.py
@@ -6,45 +6,61 @@ import pytest
 _ = test.socket.patch().start()
 _ = test.ssl.patch().start()
 
+
 @pytest.mark.xfail(reason="Known bug, see issue #2")
 def test_show():
-    assert recurse_requests.show('<body>hello</body>') == 'hello'
-    assert recurse_requests.show('he<body>llo</body>') == 'hello'
-    assert recurse_requests.show('he<body>l</body>lo') == 'hello'
-    assert recurse_requests.show('he<body>l<div>l</div>o</body>') == 'hello'
-    assert recurse_requests.show('he<body>l</div>lo') == 'hello'
-    assert recurse_requests.show('he<body>l<div>l</body>o</div>') == 'hello'
+    assert recurse_requests.show("<body>hello</body>") == "hello"
+    assert recurse_requests.show("he<body>llo</body>") == "hello"
+    assert recurse_requests.show("he<body>l</body>lo") == "hello"
+    assert recurse_requests.show("he<body>l<div>l</div>o</body>") == "hello"
+    assert recurse_requests.show("he<body>l</div>lo") == "hello"
+    assert recurse_requests.show("he<body>l<div>l</body>o</div>") == "hello"
+
 
 def test_request():
-    url = 'http://test.test/example1'
-    test.socket.respond(url, b"HTTP/1.0 200 OK\r\n" + b"Header1: Value1\r\n\r\n" + b"Body text")
+    url = "http://test.test/example1"
+    test.socket.respond(
+        url, b"HTTP/1.0 200 OK\r\n" + b"Header1: Value1\r\n\r\n" + b"Body text"
+    )
     headers, body = recurse_requests.request(url)
-    assert test.socket.last_request(url) == b'GET /example1 HTTP/1.0\r\nHost: test.test\r\n\r\n'
-    assert body == 'Body text'
-    assert headers == {'header1': 'Value1'}
+    assert (
+        test.socket.last_request(url)
+        == b"GET /example1 HTTP/1.0\r\nHost: test.test\r\n\r\n"
+    )
+    assert body == "Body text"
+    assert headers == {"header1": "Value1"}
+
 
 def test_transfer_encoding():
-    url = 'http://test.test/te'
-    test.socket.respond(url, b"HTTP/1.0 200 OK\r\n" + b"Transfer-Encoding: chunked\r\n\r\n" + b"0\r\n\r\n")
+    url = "http://test.test/te"
+    test.socket.respond(
+        url,
+        b"HTTP/1.0 200 OK\r\n" + b"Transfer-Encoding: chunked\r\n\r\n" + b"0\r\n\r\n",
+    )
     with pytest.raises(AssertionError):
         recurse_requests.request(url)
+
 
 def test_content_encoding():
-    url = 'http://test.test/ce'
-    test.socket.respond(url, b"HTTP/1.0 200 OK\r\n" + b"Content-Encoding: gzip\r\n\r\n" + b"\x00\r\n\r\n")
+    url = "http://test.test/ce"
+    test.socket.respond(
+        url,
+        b"HTTP/1.0 200 OK\r\n" + b"Content-Encoding: gzip\r\n\r\n" + b"\x00\r\n\r\n",
+    )
     with pytest.raises(AssertionError):
         recurse_requests.request(url)
 
+
 def test_ssl_support():
-    url = 'https://test.test/example2'
+    url = "https://test.test/example2"
     test.socket.respond(url, b"HTTP/1.0 200 OK\r\n\r\n")
     header, body = recurse_requests.request(url)
-    assert body == ''
+    assert body == ""
 
-    url = 'https://test.test:400/example3'
+    url = "https://test.test:400/example3"
     test.socket.respond(url, b"HTTP/1.0 200 OK\r\n\r\nHi")
     header, body = recurse_requests.request(url)
-    assert body == 'Hi'
+    assert body == "Hi"
 
     with pytest.raises(KeyError):
         recurse_requests.request("http://test.test:401/example3")
--- tests/wbetools.py
+++ tests/wbetools.py
@@ -1,22 +1,36 @@
 import dis
 
+
 def record(type, *args):
     pass
 
+
 def patch(existing_cls):
     def decorator(new_cls):
-        if isinstance(new_cls, type): # Patching classes
-            assert isinstance(existing_cls, type), f"Can't patch {existing_cls} with {new_cls}"
+        if isinstance(new_cls, type):  # Patching classes
+            assert isinstance(
+                existing_cls, type
+            ), f"Can't patch {existing_cls} with {new_cls}"
             for attr, obj in new_cls.__dict__.items():
-                if attr in ["__module__", "__dict__", "__weakref__", "__doc__"]: continue
-                assert isinstance(obj, type(record)), f"Can't patch attribute {attr} of {new_cls} to be {obj}"
+                if attr in ["__module__", "__dict__", "__weakref__", "__doc__"]:
+                    continue
+                assert isinstance(
+                    obj, type(record)
+                ), f"Can't patch attribute {attr} of {new_cls} to be {obj}"
                 setattr(existing_cls, attr, obj)
-        elif isinstance(new_cls, type(record)): # Patching functions
-            assert isinstance(existing_cls, type(record)), f"Can't patch {existing_cls} with {new_cls}"
+        elif isinstance(new_cls, type(record)):  # Patching functions
+            assert isinstance(
+                existing_cls, type(record)
+            ), f"Can't patch {existing_cls} with {new_cls}"
             for attr in dir(new_cls):
                 obj = getattr(new_cls, attr)
                 # Copies over all methods and all writable fields of functions
-                if attr in ["__code__", "__module__", "__defaults__", "__annotations__"]:
+                if attr in [
+                    "__code__",
+                    "__module__",
+                    "__defaults__",
+                    "__annotations__",
+                ]:
                     setattr(existing_cls, attr, obj)
                 elif attr == "__closure__":
                     assert obj is None, "Cannot patch using a closure"
@@ -51,10 +65,13 @@ def patch(existing_cls):
         else:
             raise ValueError(f"Cannot patch {existing_cls}")
         return existing_cls
+
     return decorator
+
 
 def js_hide(f):
     return f
+
 
 SHOW_COMPOSITED_LAYER_BORDERS = False
 USE_COMPOSITING = True
